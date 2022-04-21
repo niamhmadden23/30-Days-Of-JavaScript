5efa056f02fcbab126953fefd63161ed1d81cac2
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-21 06:50:58 +0100

    some exercises from day 3

--- 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
+++ 03_Day_Booleans_operators_date/03_day_starter/scripts/main.js
@@ -1,1 +1,21 @@
 // this is your main.js script
+
+const date = new Date();
+const year = date.getFullYear();
+console.log(year);
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getDay());
+console.log(date.getHours());
+console.log(date.getMinutes());
+const secondsNow = date.getSeconds();
+
+const oldDate = new Date(1970, 1, 1, 0, 0, 0);
+console.log(oldDate);
+
+console.log(date.getTime() - oldDate.getTime());
+
+// let base = window.prompt("enter a number");
+// let height = window.prompt("enter another number");
+// let area = 0.5 * base * height;
+// window.prompt(`the area of the triange is ${area}`);
--- 04_Day_Conditionals/04_day_starter/scripts/main.js
+++ 04_Day_Conditionals/04_day_starter/scripts/main.js
@@ -1,3 +1,26 @@
 // this is your main.js script
 
-alert('Open the browser console whenever you work on JavaScript')
+alert("Open the browser console whenever you work on JavaScript");
+
+// let age = window.prompt("please enter your age...");
+
+// if (age >= 18) {
+//   window.alert("you are older enough to drive");
+// } else {
+//   window.alert(`you have ${18 - age} years left until you can drive`);
+// }
+
+let a = 4;
+let b = 55;
+
+// if (a > b) {
+//   console.log("a is greater than b");
+// } else {
+//   console.log("a is less than b");
+// }
+
+const greaterThan = () => {
+  return a > b ? "a is greater than b" : "a is less than b";
+};
+
+console.log(greaterThan());

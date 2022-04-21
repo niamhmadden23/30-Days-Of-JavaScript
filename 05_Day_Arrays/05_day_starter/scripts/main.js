ee7a896065db2a254e9a3b08fdb0397b8070d0b7
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-21 07:34:51 +0100

    exercises from day 5

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,55 @@
-console.log(countries)
-alert('Open the browser console whenever you work on JavaScript')
-alert('Open the console and check if the countries has been loaded')
+console.log(countries);
+// alert("Open the browser console whenever you work on JavaScript");
+// alert("Open the console and check if the countries has been loaded");
+
+let arr = Array(7);
+console.log(arr.length);
+const country = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+console.log(country.at(0));
+console.log(country.length - 1);
+console.log(Math.floor(country.length / 2));
+
+const mixedData = ["Niamh", 24, true, "rice", "boxing", "reading"];
+
+console.log(mixedData.length);
+
+const itCompanies = ["facebook", "twitter", "google", "microsoft", "amazon"];
+console.log(itCompanies.length);
+console.log(itCompanies.at(0));
+console.log(itCompanies.at(2));
+console.log(itCompanies.at(length - 1));
+
+const eachWord = itCompanies.forEach((word) => {
+  console.log(word);
+});
+
+const toString = itCompanies.toString();
+itCompanies.sort();
+itCompanies.reverse();
+itCompanies.slice(0, 3);
+itCompanies.slice(itCompanies.length - 1, 3);
+console.log(itCompanies.at(itCompanies.length / 2));
+itCompanies.pop();
+
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];

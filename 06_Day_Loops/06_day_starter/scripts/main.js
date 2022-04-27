67b487e04b955aeb8165ab3dad10bbb3608a095a
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-04-27 21:17:31 +0100

    more exercises from day 6

--- 06_Day_Loops/06_day_starter/scripts/main.js
+++ 06_Day_Loops/06_day_starter/scripts/main.js
@@ -43,8 +43,60 @@ for (let number = 0; number < 5; number++) {
   string += "#";
   console.log(string);
 }
-
 console.log("exercise 5");
 for (i = 0; i <= 10; i++) {
   console.log(`${i} * ${i} = ${i * i}`);
 }
+
+console.log("i i^2 i^3");
+
+for (i = 0; i <= 10; i++) {
+  console.log(`${i}  ${i * i}  ${i * i * i}`);
+}
+
+console.log("--- EVEN NUMBERS ---");
+for (i = 0; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
+
+console.log("--- ODD NUMBERS ---");
+for (i = 0; i <= 100; i++) {
+  if (i % 2 != 0) {
+    console.log(i);
+  }
+}
+
+console.log("--- THE SUM OF NUMBERS ---");
+
+let sum = 0;
+for (i = 0; i <= 100; i++) {
+  sum = i + sum;
+  console.log(sum);
+}
+console.log("--- THE SUM OF EVEN NUMBERS ---");
+
+let result = 0;
+for (i = 0; i <= 100; i++) {
+  if (i % 2 === 0) {
+    result = i + result;
+    console.log(result);
+  }
+}
+
+console.log("--- THE SUM OF ODD NUMBERS ---");
+
+let total = 0;
+for (i = 0; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    total = i + total;
+    console.log(total);
+  }
+}
+console.log("---Random Number in Array---");
+let array = [];
+for (let i = 0; i < 5; i++) {
+  array.push(Math.floor(Math.random() * 100 + 1));
+}
+console.log(array);

a1228e497852a77a105109a7a889db772725e5ac
niamhmadden23 <83446408+niamhmadden23@users.noreply.github.com>
2022-05-17 19:36:13 +0100

    more exercises

--- 07_Day_Functions/07_day_starter/scripts/main.js
+++ 07_Day_Functions/07_day_starter/scripts/main.js
@@ -1,2 +1,61 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+function printName(firstName, lastName) {
+  console.log(`${firstName} ${lastName}`);
+}
+
+printName("Niamh", "Madden");
+
+function sumOfNums(a, b) {
+  console.log(a + b);
+}
+
+sumOfNums(200, 2000);
+
+function areaOfRectangle(length, width) {
+  let area = length * width;
+  console.log(area);
+}
+
+areaOfRectangle(10, 4);
+
+function perimeterOfRectangle(length, width) {
+  let perimeter = 2 * (length + width);
+  console.log(perimeter);
+}
+
+perimeterOfRectangle(4, 4);
+
+function volumeOfRectangle(length, height, width) {
+  let volume = length * width * height;
+  console.log(volume);
+}
+
+volumeOfRectangle(2, 2, 2);
+
+const printArray = (array) => {
+  array.forEach((element) => {
+    console.log(element);
+  });
+};
+
+printArray(["Niamh", "Mabel"]);
+
+function showDateTime() {
+  let day = new Date();
+  let date = day.getDate();
+  let month = day.getMonth();
+  let year = day.getFullYear();
+
+  let time = day.getHours();
+  let minutes = day.getMinutes();
+  console.log(`0${date}/0${month + 1}/${year} ${time}:${minutes}`);
+}
+
+showDateTime();
+
+console.log("---swap values---");
+
+function swapValue(num1, num2) {
+  return ([num1, num2] = [num2, num1]);
+}
+
+console.log(swapValue(2, 3));
--- 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
+++ 10_Day_Sets_and_Maps/10_day_starter/scripts/main.js
@@ -1,2 +1,64 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const newSet = new Set();
+
+console.log(newSet);
+
+const languages = [
+  "English",
+  "Finnish",
+  "English",
+  "French",
+  "Spanish",
+  "English",
+  "French",
+];
+const setOfLanguages = new Set(languages);
+console.log(setOfLanguages);
+for (language of setOfLanguages) {
+  console.log(language);
+}
+setOfLanguages.forEach((language) => {
+  console.log(`for each ${language}`);
+});
+
+setOfLanguages.add("Italian");
+console.log(setOfLanguages.size);
+console.log(setOfLanguages);
+console.log(setOfLanguages.delete("Finnish"));
+console.log(setOfLanguages);
+console.log(setOfLanguages.has("Portugese"));
+console.log(setOfLanguages.has("Italian"));
+setOfLanguages.clear();
+console.log(setOfLanguages);
+
+const emptySet = new Set();
+console.log(emptySet);
+for (let i = 0; i < 10; i++) {
+  emptySet.add(i);
+}
+console.log(emptySet);
+emptySet.delete(4);
+console.log(emptySet);
+
+const newMap = new Map();
+newMap.set("England", 7);
+console.log(newMap);
+
+const a = [4, 5, 8, 9];
+const b = [3, 4, 5, 7];
+
+const union = new Set([...a, ...b]);
+console.log(union);
+
+const x = new Set([4, 5, 8, 9]);
+const y = new Set([3, 4, 5, 7]);
+
+const intersection = new Set([...x].filter((number) => y.has(number)));
+console.log(intersection);
+
+const difference = new Set([...x].filter((number) => !y.has(number)));
+console.log(difference);
+
+console.log(countries);
+
+let languagesInObj = countries.map((language) => language.languages);
+console.log(languagesInObj);
